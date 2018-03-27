b7359406063a415e216ed4fee17bc262d10ccbb0
raphaki <raphael.kirchner@yahoo.de>
2018-03-27 16:55:22 +0200

    Initial commit

---
+++ app/src/main/java/com/example/rapha/sundaybaking/recipesActivity.java
@@ -0,0 +1,13 @@
+package com.example.rapha.sundaybaking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class recipesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes);
+    }
+}
